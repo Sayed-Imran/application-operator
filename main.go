8636f3b3f006d20ee0335bb066fbad7475570a97
Sayed-Imran <sayedimran00786@gmail.com>
2024-03-11 17:15:44 +0000

    Creating v1alpha2 version of Application

--- main.go
+++ main.go
@@ -32,6 +32,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	apiv1alpha1 "github.com/Sayed-Imran/application-operator/api/v1alpha1"
+	apiv1alpha2 "github.com/Sayed-Imran/application-operator/api/v1alpha2"
 	"github.com/Sayed-Imran/application-operator/controllers"
 	//+kubebuilder:scaffold:imports
 )
@@ -45,6 +46,7 @@ func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
 	utilruntime.Must(apiv1alpha1.AddToScheme(scheme))
+	utilruntime.Must(apiv1alpha2.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
